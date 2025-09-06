35e2c5c3c0fd5836bb8b5596d5c8dfc7b55e744a
vohrr <matthew.d.pingel@gmail.com>
2025-09-05 20:15:48 -0400

    fix bug in registration, resolved openapi config conflict

--- careconnect2025/backend/core/src/main/java/com/careconnect/config/OpenApiConfig.java
+++ careconnect2025/backend/core/src/main/java/com/careconnect/config/OpenApiConfig.java
@@ -135,8 +135,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     // Configuration is handled through annotations
     // Bean configuration commented out temporarily to avoid conflicts
-    
-    
+    /* 
     @Bean
     public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
         return new io.swagger.v3.oas.models.OpenAPI()
@@ -148,5 +147,6 @@ public class OpenApiConfig {
                 .url("http://localhost:8080")
                 .description("Development Server"));
     }
+                */
     
 }
